697c3c56ec03bc1e89b6f7d8544c528f1b5c6d24
Lucid-Synth <uacode75@gmail.com>
2026-05-16 16:24:57 +0530

    created upload api routes and added unpdf to extract the text from pdf

---
+++ app/api/upload/pdf/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { extractText,getDocumentProxy } from "unpdf";
+
+const buffer = await fetch('https://pdfobject.com/pdf/sample.pdf')
+  .then(res => res.arrayBuffer())
+
+const pdf = await getDocumentProxy(new Uint8Array(buffer))
+const { text } = await extractText(pdf)
+
+console.log(text)
+
+
+export async function GET(request: Request){
+    return NextResponse.json({message: "Server is running!!"},{status:200});
+}
