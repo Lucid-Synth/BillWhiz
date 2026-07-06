f5305d18415e40ede3cf4ee06c253808a6060e82
Lucid-Synth <uacode75@gmail.com>
2026-07-06 10:30:15 +0530

    Added the production mail

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,14 +1,29 @@
 import InvoiceEmail from '@/app/dashboard/email/InvoiceEmail';
+import { auth } from '@/app/lib/auth';
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
+export async function POST(req: Request,res: NextResponse) {
   try {
+
+    const session = await auth.api.getSession({
+        headers: await headers(),
+      });
+
+      if (!session?.user?.email) {
+          return NextResponse.json(
+            { success: false, error: "Unauthorized" },
+            { status: 401 }
+          );
+        }
+
     const invoice = await req.json();
     const { data, error } = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: 'jedepen345@fivejm.com',
+      from: 'BillWhiz <team@app.ujjwaldev.site>',
+      to: session?.user.email,
       subject: 'Invoice Analysis',
       react: InvoiceEmail({ invoice: invoice}),
     });
