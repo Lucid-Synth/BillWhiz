0a8f1ca136f2fbcf518f4d9fcbab102f581cce05
Lucid-Synth <uacode75@gmail.com>
2026-07-06 10:48:31 +0530

    Remove unused response parameter from POST function

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -6,7 +6,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request,res: NextResponse) {
+export async function POST(req: Request) {
   try {
 
     const session = await auth.api.getSession({
